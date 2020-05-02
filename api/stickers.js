12f9f19467753775a58604c4f523724fa28aec60
iArtz <dev@iartz.io>
2020-05-03 00:02:13 +0700

    🐱 Add api folder

---
+++ api/stickers.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const route = express.Router()
+
+route.get('/', (req, res) => {
+    res.json({
+        message: '✅'
+    })
+})
+
+module.exports = route
--- app.js
+++ app.js
@@ -1,28 +1,32 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors')
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
 
-var app = express();
+const app = express()
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+const stickers = require('./api/stickers')
+
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+
+app.use('/api/v1/stickers/', stickers)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
-});
+  next(createError(404))
+})
 
 // error handler
 app.use(function (err, req, res, next) {
-  res.status(err.status || 500);
+  res.status(err.status || 500)
   res.json({
     message: err.message,
     error: req.app.get('env') === 'development' ? err : {},
-  });
-});
+  })
+})
 
-module.exports = app;
+module.exports = app
