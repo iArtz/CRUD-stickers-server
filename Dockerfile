f882413f146b8a118eee6deb7699323f931782eb
iArtz <Nightcore@me.com>
2020-05-18 23:50:25 +0700

    🐱 Update docker compose ci

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@ FROM node:alpine
 
 WORKDIR /usr/src/app
 
+RUN apk add postgresql-client
+
 COPY package.json .
 
 RUN npm install
--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,16 @@
 // Update with your config settings.
 
 module.exports = {
-  development: {
+  production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
+  },
+  development: {
+    client: 'pg',
+    connection: process.env.DATABASE_DEV_URL,
   },
   test: {
     client: 'pg',
     connection: process.env.DATABASE_TEST_URL,
   },
-};
+}
